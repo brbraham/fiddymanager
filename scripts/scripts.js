e6abfc18716c0b98d0120ccf58cdee2dca841a69
Samantha Brown <smb173@uakron.edu>
2024-02-12 18:15:47 -0500

    adding login pages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -30,7 +30,7 @@ function addSam() {
     username,
     pass,
     created: createdFormatted,
-    expires: expiresFormatted
+    expires: expiresFormatted,
   };
   sams.push(sam);
 
@@ -75,7 +75,7 @@ function displaySams() {
   // Display the sorted sams
   sams.forEach((sam, index) => {
     const row = tableBody.insertRow();
-    row.classList.add('unblur-on-hover');
+    row.classList.add("unblur-on-hover");
     Object.values(sam).forEach((text) => {
       const cell = row.insertCell();
       cell.innerText = text;
@@ -85,7 +85,8 @@ function displaySams() {
     const passCell = row.getElementsByTagName("td")[2];
     if (passCell.innerText.match(/[a-zA-Z0-9]{8}/)) {
       passCell.classList.add("blur");
-    } else if (passCell.innerText.trim() !== "") { // Check if the password is not empty
+    } else if (passCell.innerText.trim() !== "") {
+      // Check if the password is not empty
       passCell.classList.add("blur");
     }
 
@@ -102,7 +103,8 @@ sortingSelect.addEventListener("change", function () {
 });
 
 function generatePassword() {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-";
   let password = "";
   for (let i = 0; i < 9; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
@@ -138,43 +140,45 @@ let interval; // Global variable to keep track of the interval ID
 const inactivityDuration = 10 * 1000; // 10 seconds for demonstration
 const timerDisplay = document.querySelector("#time"); // Assuming this is your timer display element
 function showTimer() {
-    if (timerDisplay) timerDisplay.style.display = 'block'; // Show the timer display
+  if (timerDisplay) timerDisplay.style.display = "block"; // Show the timer display
 }
 function hideTimer() {
-    if (timerDisplay) timerDisplay.style.display = 'none'; // Hide the timer display
+  if (timerDisplay) timerDisplay.style.display = "none"; // Hide the timer display
 }
 function startTimer(duration) {
-    let timer = duration, minutes, seconds;
-    showTimer(); // Make sure to show the timer when starting the countdown
-    // Clear existing interval to ensure no multiple intervals running
-    clearInterval(interval);
-    interval = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        if (timerDisplay) timerDisplay.textContent = minutes + ":" + seconds;
-        if (--timer < 0) {
-            clearInterval(interval);
-            showModal(); // Your action when the timer expires
-            hideTimer(); // Hide the timer display once the countdown is complete
-        }
-    }, 1000);
+  let timer = duration,
+    minutes,
+    seconds;
+  showTimer(); // Make sure to show the timer when starting the countdown
+  // Clear existing interval to ensure no multiple intervals running
+  clearInterval(interval);
+  interval = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    if (timerDisplay) timerDisplay.textContent = minutes + ":" + seconds;
+    if (--timer < 0) {
+      clearInterval(interval);
+      showModal(); // Your action when the timer expires
+      hideTimer(); // Hide the timer display once the countdown is complete
+    }
+  }, 1000);
 }
 function resetTimer() {
-    clearTimeout(timeout); // Clear the timeout
-    clearInterval(interval); // Also clear the interval to stop the countdown
-    hideTimer(); // Hide the timer immediately upon reset
-  
-    timeout = setTimeout(function () {
-        startTimer(inactivityDuration / 1000);
-    }, inactivityDuration);
+  clearTimeout(timeout); // Clear the timeout
+  clearInterval(interval); // Also clear the interval to stop the countdown
+  hideTimer(); // Hide the timer immediately upon reset
+
+  timeout = setTimeout(function () {
+    startTimer(inactivityDuration / 1000);
+  }, inactivityDuration);
 }
 function setupInactivityTimer() {
-    window.onload = resetTimer;
-    document.onmousemove = resetTimer;
-    document.onkeypress = resetTimer;
-    document.onclick = resetTimer;
+  window.onload = resetTimer;
+  document.onmousemove = resetTimer;
+  document.onkeypress = resetTimer;
+  document.onclick = resetTimer;
 
   // Reset the popup when the user becomes active
   document.onmousemove = function () {
@@ -188,15 +192,15 @@ function setupInactivityTimer() {
   };
 
   // Initially hide the timer until it's needed
-    hideTimer();
+  hideTimer();
 }
 document.addEventListener("DOMContentLoaded", function () {
-    loadData();
-    document.getElementById("okButton").onclick = function () {
-      hideModal();
-      resetTimer();
-    }
-    setupInactivityTimer();
+  loadData();
+  document.getElementById("okButton").onclick = function () {
+    hideModal();
+    resetTimer();
+  };
+  setupInactivityTimer();
 });
 
 function showModal() {
@@ -217,7 +221,9 @@ function resetPopup() {
   var modal = document.getElementById("myModal");
   if (modal.classList.contains("show")) {
     modal.classList.remove("show");
-    document.querySelector(".content-wrapper").classList.remove("blur-background");
+    document
+      .querySelector(".content-wrapper")
+      .classList.remove("blur-background");
   }
   if (document.getElementById("popup").style.display === "block") {
     closePopup();
@@ -232,7 +238,6 @@ function openPopup() {
 function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
-
 
 // Set initial state of buttons
 passwordBtn.style.backgroundColor = "";
@@ -260,9 +265,15 @@ function addSam() {
   }
 
   let pass;
-  if (document.getElementById("passwordBtn").style.backgroundColor === "rgb(57, 183, 255)") {
+  if (
+    document.getElementById("passwordBtn").style.backgroundColor ===
+    "rgb(57, 183, 255)"
+  ) {
     pass = generatePassword(1).toUpperCase();
-  } else if (document.getElementById("passphraseBtn").style.backgroundColor === "rgb(57, 183, 255)") {
+  } else if (
+    document.getElementById("passphraseBtn").style.backgroundColor ===
+    "rgb(57, 183, 255)"
+  ) {
     pass = generatePassphrase();
   } else {
     alert(
@@ -283,7 +294,7 @@ function addSam() {
     username,
     pass,
     created: createdFormatted,
-    expires: expiresFormatted
+    expires: expiresFormatted,
   };
   sams.push(sam);
 
@@ -299,14 +310,18 @@ function addSam() {
 }
 
 function logout() {
-  firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-    // Redirect the user to the login page or perform any other action after logout.
-    window.location.href = "login.html";
-  }).catch(function(error) {
-    // An error happened.
-    console.error("Logout error:", error);
-  });
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      // Sign-out successful.
+      // Redirect the user to the login page or perform any other action after logout.
+      window.location.href = "login.html";
+    })
+    .catch(function (error) {
+      // An error happened.
+      console.error("Logout error:", error);
+    });
 }
 
 const characters =
@@ -619,10 +634,164 @@ const wordOptions = [
   "extent",
   "addition",
   "station",
-  "window"
+  "window",
 ];
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvaqJ-s7imh09Xp95rbGD8O5twjMCfJPk",
+  authDomain: "capstoneproject-71f4c.firebaseapp.com",
+  projectId: "capstoneproject-71f4c",
+  storageBucket: "capstoneproject-71f4c.appspot.com",
+  messagingSenderId: "359918716352",
+  appId: "1:359918716352:web:4a1f65791b99eacb797425",
+  measurementId: "G-9ZGHGKVENP",
+};
 
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+const auth = getAuth(firebaseApp);
 
+// Function to perform login authentication
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
 
+    // Retrieve email and password from form
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Sign in with email and password using Firebase Authentication
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        // Redirect to dashboard upon successful login
+        window.location.href = "Dashboard.html"; // Redirect to dashboard upon successful login
+      })
+
+      .catch((error) => {
+        // Handle login errors
+        const errorCode = error.code;
+        let errorMessage;
+
+        switch (errorCode) {
+          case "auth/user-not-found":
+            errorMessage =
+              "User not found. Please check your email and try again.";
+            break;
+          case "auth/wrong-password":
+            errorMessage = "Incorrect password. Please try again.";
+            break;
+          // You can add more cases to handle other error codes as needed
+          default:
+            errorMessage = "An error occurred. Please try again later.";
+            break;
+        }
+
+        document.getElementById("errorText").innerText = errorMessage;
+        document.getElementById("errorText").style.display = "block";
+      });
+  });
+// Function to switch to new user form
+document
+  .querySelector(".new-user-text")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("loginContainer").style.display = "none";
+    document.getElementById("newUserContainer").style.display = "block";
+  });
+
+// Function to switch back to login form
+document
+  .querySelector(".back-to-login")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("newUserContainer").style.display = "none";
+    document.getElementById("loginContainer").style.display = "block";
+  });
+
+// Function to handle forgot password
+document
+  .querySelector(".forgot-password")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const email = prompt("Please enter your email to reset password:");
+    if (email) {
+      sendPasswordResetEmail(auth, email)
+        .then(() => {
+          // Password reset email sent
+          alert("Password reset email sent. Please check your email.");
+        })
+        .catch((error) => {
+          // Handle errors
+          console.error("Error sending password reset email:", error.message);
+          alert("Error sending password reset email. Please try again.");
+        });
+    }
+  });
+
+// Function to create new user
+document
+  .getElementById("createUserForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission
+    const username = document.getElementById("newUsername").value;
+    const email = document.getElementById("newUserEmail").value;
+    const password = document.getElementById("newUserPassword").value;
+    const confirmPassword = document.getElementById(
+      "confirmNewUserPassword"
+    ).value;
+
+    // Validate password strength
+    const passwordRegex =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
+    if (!passwordRegex.test(password)) {
+      document.getElementById("newUserErrorText").innerText =
+        "Password must be between 8-16 characters long and contain at least one number and one special character (!@#$%^&*).";
+      return;
+    }
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      document.getElementById("newUserErrorText").innerText =
+        "Passwords do not match. Please try again.";
+      return;
+    }
+
+    // Create new user with Firebase authentication
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // User registered successfully
+        var user = userCredential.user;
+        console.log("User registered:", user.uid);
+        // Display success message
+        document.getElementById("successMessage").innerText =
+          "User registered successfully!";
+        document.getElementById("successMessage").style.display = "block";
+        // Redirect to login page after a delay (e.g., 3 seconds)
+        setTimeout(() => {
+          window.location.href = "login.html";
+        }, 3000); // 3000 milliseconds = 3 seconds
+      })
+      .catch((error) => {
+        // Handle registration errors
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorMessage);
+        // You can display the error message to the user or perform other actions as needed
+        document.getElementById("errorText").innerText = errorMessage;
+        document.getElementById("errorText").style.display = "block";
+      });
+  });
